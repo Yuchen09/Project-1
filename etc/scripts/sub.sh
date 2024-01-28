84c2b670b70dec09a6305708d261d2d8c7fe6407
Yuchen09 <156837073+Yuchen09@users.noreply.github.com>
2024-01-28 16:17:36 -0600

    Add sub.sh

---
+++ etc/scripts/sub.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function sub() {
+    local topic_name
+    local file_name
+
+    read -p "Type the topic_name: " topic_name
+    read -p "Type the file_name.csv: " file_name
+
+    # publish ros2 topic, press CTRL + C  to stop publishing
+    ros2 topic pub  --rate 10 $topic_name geometry_msgs/msg/Pose
+
+# save topics
+    echo "$topic_name"  > "$file_name"
+    nano $file_name
+
+}
+
+# Call the function
+sub
+
